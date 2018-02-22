7e630c8391bef9e36a341564ae636dfe43928d75
Jack Sullivan <sullivanjack87@gmail.com>
2018-02-22 15:27:41 -0600

    EmployeeRestController

--- src/main/java/edu/uark/controllers/EmployeeRestController.java
+++ src/main/java/edu/uark/controllers/EmployeeRestController.java
@@ -12,7 +12,7 @@ public class EmployeeRestController {
 	@ResponseBody
 	@RequestMapping(value = "/test", method = RequestMethod.GET)
 	public String test() {
-		return "/api/employee/test I have no idea what I am doing still";
+		return "/api/employee/test I have no idea what I am doing still, like still";
 	}
 	
 }
